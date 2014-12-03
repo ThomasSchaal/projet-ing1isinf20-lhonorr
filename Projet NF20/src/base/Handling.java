d4a086a11ee70879010ef46295a8353c55473501
thomas.schaal068@gmail.com <thomas.schaal068@gmail.com@e12ad3f4-e9a0-b1d5-b4c3-03391ce63b50>
2014-12-03 08:34:29 +0000

    Classe de tri heapsort pour Kruskal fonctionne, attention elle contient un main

---
+++ Projet NF20/src/base/Handling.java
@@ -0,0 +1,24 @@
+package base;
+
+
+/**
+ * Classe abstraite - gestion de cas particuliers hors arbre
+ * @author Robin
+ *
+ */
+
+public class Handling {
+
+	 /**
+     * Retourne Vrai si la chaîne de caractères est null, vide ou avec
+     * uniquement des espaces
+     * 
+     * @param s
+     *         Chaîne de caractères
+     * @return Vrai si la chaîne de caractères est null, vide ou avec uniquement
+     *         des espaces ; Sinon Faux
+     */
+    public static boolean nullOrSpacesOrEmpty(String s) {
+            return s == null || s.trim().equalsIgnoreCase("");
+    }
+}
